be89ef459389e6a9e436e30457fa748ce8f443fe
dd-buntar <ckonyginad@yandex.ru>
2025-10-09 11:15:51 +0300

    feat(inmemory/storehouse): add implenent methods to inmemory rep

--- src/main/java/com/github/dd_buntar/goods_warehouse/domain/entities/Storehouse.java
+++ src/main/java/com/github/dd_buntar/goods_warehouse/domain/entities/Storehouse.java
@@ -1,17 +1,17 @@
 package com.github.dd_buntar.goods_warehouse.domain.entities;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Storehouse {
     private Long stockId;
     private Long shipmentId;
     private Integer quantity;
     private Long locationId;
-
-
 }
--- src/main/java/com/github/dd_buntar/goods_warehouse/infrastructure/inmemory/InMemoryStorehouseRepository.java
+++ src/main/java/com/github/dd_buntar/goods_warehouse/infrastructure/inmemory/InMemoryStorehouseRepository.java
@@ -3,36 +3,53 @@ package com.github.dd_buntar.goods_warehouse.infrastructure.inmemory;
 import com.github.dd_buntar.goods_warehouse.domain.entities.Storehouse;
 import com.github.dd_buntar.goods_warehouse.domain.repositories.StorehouseRepository;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class InMemoryStorehouseRepository implements StorehouseRepository {
     private final Map<Long, Storehouse> storehouseStorage = new HashMap<>();
+    private final AtomicLong idCounter = new AtomicLong(1);
 
     @Override
     public Storehouse create(Storehouse entity) {
-        return null;
+        Long nextId = idCounter.getAndIncrement();
+
+        Storehouse newStorehouse = Storehouse.builder()
+                .stockId(nextId)
+                .shipmentId(entity.getShipmentId())
+                .quantity(entity.getQuantity())
+                .locationId(entity.getLocationId())
+                .build();
+
+        storehouseStorage.put(nextId, newStorehouse);
+        return newStorehouse;
     }
 
     @Override
-    public Optional<Storehouse> findById(Long aLong) {
-        return Optional.empty();
+    public Optional<Storehouse> findById(Long id) {
+        return Optional.ofNullable(storehouseStorage.get(id));
     }
 
     @Override
     public List<Storehouse> findAll() {
-        return null;
+        return new ArrayList<>(storehouseStorage.values());
     }
 
     @Override
     public Optional<Storehouse> update(Storehouse entity) {
+        if (entity.getStockId() == null) {
+            return Optional.empty();
+        }
+
+        if (storehouseStorage.containsKey(entity.getStockId())) {
+            storehouseStorage.put(entity.getStockId(), entity);
+            return Optional.of(entity);
+        }
         return Optional.empty();
     }
 
     @Override
-    public boolean deleteById(Long aLong) {
-        return false;
+    public boolean deleteById(Long id) {
+        return storehouseStorage.remove(id) != null;
     }
 }
